25774b2e0974070192d11f5acf234bfaeeed3695
Vincent PLUCHARD <vpluchar@e2r5p22.42.fr>
2016-08-24 11:43:24 +0200

    42

---
+++ J08/ex01/ft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vpluchar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/08/24 09:59:00 by vpluchar          #+#    #+#             */
+/*   Updated: 2016/08/24 10:03:32 by vpluchar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+void	ft_swap(int *a, int *b);
+int		ft_strcmp(char *s1, char *s2);
+int		ft_strlen(char *str);
+#endif
---
+++ J08/ex02/ft_boolean.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vpluchar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/08/24 10:04:53 by vpluchar          #+#    #+#             */
+/*   Updated: 2016/08/24 11:15:07 by vpluchar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+# include <unistd.h>
+# define EVEN(x)		((x % 2) == 0) ? TRUE : FALSE
+# define EVEN_MSG		"I have an even number of arguments\n"
+# define ODD_MSG		"I have an odd number of arguments\n"
+# define SUCCESS		0
+# define TRUE			1
+# define FALSE			0
+
+typedef	int		t_bool;
+
+#endif
